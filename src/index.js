9bbf3a7e7dda6c0ae889eb467bbfadd0143587b4
Temple <el.temple@gmail.com>
2020-04-04 15:30:54 +0200

    Archivo index.js tras los pasos 5, 6 y 7

---
+++ src/index.js
@@ -0,0 +1,5 @@
+   window.addEventListener('DOMContentLoaded', (event) => {
+     let h1 = document.createElement('h1');
+     h1.innerHTML = 'Este es mi primer proyecto <em>webpack</em>!';
+     document.body.appendChild(h1);
+   }); 
