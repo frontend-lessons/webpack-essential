c1ee46c772599199ee550517930fcbbab44aa0dc
Temple <el.temple@gmail.com>
2020-04-05 11:37:10 +0200

    Archivo webpack.module.js tras el paso 5

--- config/webpack/webpack.module.js
+++ config/webpack/webpack.module.js
@@ -1,1 +1,10 @@
-module.exports = 
+module.exports = { 
+    unknownContextRequest: '.',
+    unknownContextRecursive: true,
+    unknownContextCritical: true,
+    exprContextRequest: '.',
+    exprContextRecursive: true,
+    exprContextCritical: true,
+    wrappedContextRegExp: /.*/,
+    wrappedContextRecursive: true,
+}
