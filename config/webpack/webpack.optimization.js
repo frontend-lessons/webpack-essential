b890ba60b0981cff715d228fe9c94d08608bcda0
Temple <el.temple@gmail.com>
2020-04-05 11:41:46 +0200

    Archivo webpack.optimization.js tras el paso 6

--- config/webpack/webpack.optimization.js
+++ config/webpack/webpack.optimization.js
@@ -1,1 +1,28 @@
-module.exports = 
+module.exports = { 
+    removeAvailableModules: true,
+    removeEmptyChunks: true,
+    mergeDuplicateChunks: true,
+    flagIncludedChunks: true,
+    occurrenceOrder: true,
+    sideEffects: true,
+    providedExports: true,
+    usedExports: true,
+    concatenateModules: true,
+    splitChunks:{
+        hidePathInfo: true,
+        chunks: 'async',
+        minSize: 30000,
+        minChunks: 1,
+        maxAsyncRequests: 5,
+        automaticNameDelimiter: '~',
+        automaticNameMaxLength: 109,
+        maxInitialRequests: 3,
+        name: true,
+        cacheGroups: {} 
+    },
+    noEmitOnErrors: true,
+    checkWasmTypes: true,
+    minimize: true,
+    minimizer: [ ],
+    nodeEnv: 'production' 
+}
