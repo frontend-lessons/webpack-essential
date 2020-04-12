8e38727da4ae9be823723c9f2e474fdb36216314
Temple <el.temple@gmail.com>
2020-04-12 12:14:55 +0200

    Archivo webpack.plugins.js tras el paso 15

--- config/webpack/webpack.plugins.js
+++ config/webpack/webpack.plugins.js
@@ -1,3 +1,4 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
