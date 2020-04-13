d055adb3af394f973e669c2b73bdb9c33f9c0621
Temple <el.temple@gmail.com>
2020-04-13 17:41:09 +0200

    Archivo webpack.plugins.js tras el paso 29

--- config/webpack/webpack.plugins.js
+++ config/webpack/webpack.plugins.js
@@ -2,7 +2,14 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-
+const path = require('path');
+const getIncrementalFileName = function(targetPath,absolutePath){
+    getIncrementalFileName.offset++
+    let extension = path.extname(targetPath)
+    let target_dir = path.dirname(targetPath)
+    return path.join(target_dir,getIncrementalFileName.offset.toString()+extension)
+}
+getIncrementalFileName.offset = 0;
 
 module.exports = [
     new CleanWebpackPlugin(),
