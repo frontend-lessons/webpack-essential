b64b0bdfdd90c7f029ec40ca1e40da3884a4c967
Temple <el.temple@gmail.com>
2020-04-13 11:12:16 +0200

    Archivo index.js tras el paso 21

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-   import 'bulma/css/bulma.min.css';   
+   import 'bulma/css/bulma.min.css';
+   import './css/style.css';   
    window.addEventListener('DOMContentLoaded', (event) => {
      let h1 = document.createElement('h1');
      h1.innerHTML = 'Este es mi primer proyecto <em>webpack</em>!';
