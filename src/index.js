f1a9ad326a78c2bc7c2be1a6cb87b90d4f3de780
Temple <el.temple@gmail.com>
2020-04-20 19:33:43 +0200

    Archivo index.js tras el paso 18

--- src/index.js
+++ src/index.js
@@ -4,20 +4,9 @@ import "./css/bulma.sass"
 import "./css/style.css"   
 
 window.addEventListener("DOMContentLoaded", () => {
-    let h1 = document.createElement("h1")
-    h1.innerHTML = "Este es mi primer proyecto <em>webpack</em>!"
-    document.body.appendChild(h1)
-    showBanner(WebpackLogo,"Webpack corporative logo","https://webpack.js.org","Web oficial de webpack",3000,document.querySelector("#main"))
+  let h1 = document.createElement("h1")
+  h1.innerHTML = "Este es mi primer proyecto <em>webpack</em>!"
+  document.body.appendChild(h1)
+  showBanner(WebpackLogo,"Webpack corporative logo","https://webpack.js.org","Web oficial de webpack",3000,document.querySelector("#main"))
 }) 
 
-window.addEventListener("DOMContentLoaded", () => {
-    let anchor = document.createElement("a")
-    let Img = new Image()
-    Img.src = WebpackLogo
-    Img.setAttribute("alt","Webpack corporative logo")
-    anchor.appendChild(Img)
-    anchor.href="https://webpack.js.org"
-    let logo = document.querySelector("#main #logo")
-    document.body.appendChild(logo)
-    logo.replaceWith(anchor)
-}) 
