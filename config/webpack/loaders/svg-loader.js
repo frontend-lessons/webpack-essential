ac81cc25ac6337e453c8806ba1f49171cd9c2107
Temple <el.temple@gmail.com>
2020-05-06 10:28:08 +0200

    Archivo svg-loader.js tras el paso 27

---
+++ config/webpack/loaders/svg-loader.js
@@ -0,0 +1,21 @@
+import { getOptions } from 'loader-utils';
+import validateOptions from 'schema-utils';
+
+const schema = {
+  type: 'object',
+  properties: {
+    test: {
+      type: 'string'
+    }
+  }
+};
+
+export default function(source) {
+  const options = getOptions(this);
+
+  validateOptions(schema, options, 'Example Loader');
+
+  // Apply some transformations to the source...
+
+  return `export default ${ JSON.stringify(source) }`;
+}
